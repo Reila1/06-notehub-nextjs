a7e35e14f497b4590c66647d159dfc40a9cb18fd
Sofiia Moiseienko <sofiamouseenko@gmail.com>
2026-02-08 21:26:52 +0300

    Fix file names

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,6 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
-import NotesClient from './NotesClient';
+import NotesClient from './Notes.client';
 export default async function NotesPage() {
   const queryClient = new QueryClient();
   
